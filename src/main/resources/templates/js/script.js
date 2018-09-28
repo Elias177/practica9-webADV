581fe796dec16ae831ce3835f1987146aa46283f
dbzronald <dbzronald_3@hotmail.com>
2018-09-28 02:17:25 -0400

    Cambio a yandex maps

--- src/main/resources/templates/js/script.js
+++ src/main/resources/templates/js/script.js
@@ -7,7 +7,7 @@ var locati = {};
 var loc = {};
 
 
-
+var map;
 var geoSettings = {
     enableHighAccuracy: true,
     timeout: 6000,
@@ -47,6 +47,10 @@ dataBase.onerror = function (e) {
 };
 
 window.onload = function() {
+    map = new ymaps.Map("map", {
+        center: [19.44, -70.677],
+        zoom: 7
+    });
     encuestasListado();
 };
 
@@ -94,43 +98,26 @@ function encuestasListado() {
 
             cursor.continue();
 
-        }else {
-            console.log("La cantidad de registros recuperados es: "+contador);
         }
     };
 
     data.oncomplete = function () {
 
         try {
-            var map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 10,
-                center: new google.maps.LatLng(19.44, -70.677),
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            });
 
-            var infowindow = new google.maps.InfoWindow();
-            var marker, i = 0;
+            var place;
+
             for (var key in encuestaDatos) {
+                place = new ymaps.Placemark([encuestaDatos[key].latitud, encuestaDatos[key].longitud], { hintContent: 'Encuesta '+encuestaDatos[key].id, balloonContent: encuestaDatos[key].nombre });
+                map.geoObjects.add(place);
 
-                marker = new google.maps.Marker({
-                    position: new google.maps.LatLng(parseFloat(encuestaDatos[key].latitud), parseFloat(encuestaDatos[key].longitud)),
-                    map: map
-                });
-
-                google.maps.event.addListener(marker, 'click', (function(marker, i) {
-                    return function() {
-                        infowindow.setContent(encuestaDatos[key].nombre);
-                        infowindow.open(map, marker);
-                    }
-                })(marker, i));
-                i = i +1;
             }
             hayTabla(encuestaDatos);
             return encuestaDatos;
 
         } catch (e) {
             hayTabla(encuestaDatos);
-            console.log("El mapa no esta disponible offline")  ;                   // "@Scratchpad/2:2:7\n"
+            alert("El mapa no esta disponible offline")  ;
         }
 
 
